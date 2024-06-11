8e0f7b806478f663d662d1bed4fb8aa19efcdd85
rizkiirwn <padiyacantik123@gmail.com>
2024-06-11 07:07:20 +0700

    zero

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'username' => 'kasir 1',
+            'email' => 'kasir_1@gmail.com',
+            'password' => Hash::make('123'),
+            'role' => 'staff',
+        ]);
+    }
+}
